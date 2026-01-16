5cc1e92fa7c2e1055013d1cd7624602a97f22d3f
Arto Bendiken <arto@bendiken.net>
2026-01-16 07:58:47 -0600

    Define top-level crate features.

--- lib/flows/src/lib.rs
+++ lib/flows/src/lib.rs
@@ -8,3 +8,30 @@ extern crate alloc;
 
 #[cfg(feature = "std")]
 extern crate std;
+
+#[cfg(feature = "audio")]
+pub use flows_audio as audio;
+
+#[cfg(feature = "derive")]
+pub use flows_derive as derive;
+
+#[cfg(feature = "hash")]
+pub use flows_hash as hash;
+
+#[cfg(feature = "image")]
+pub use flows_image as image;
+
+#[cfg(feature = "io")]
+pub use flows_io as io;
+
+#[cfg(feature = "json")]
+pub use flows_json as json;
+
+#[cfg(feature = "math")]
+pub use flows_math as math;
+
+#[cfg(feature = "rand")]
+pub use flows_rand as rand;
+
+#[cfg(feature = "text")]
+pub use flows_text as text;
